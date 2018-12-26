4ae5aed35a8febf75987ddbfe74bfd101943f2d0
liuhongbo <liuhongbo@finance.com>
2018-12-26 11:31:13 +0800

    测试下

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var name = "刘红波";
+ var role = "iOS";
+ var formatedName = HTMLheaderName.replace("%data%",name);
+ var formatedRole = HTMLheaderRole.replace("%data%",role);
+ $("#header").prepend(formatedRole);
+ $("#header").prepend(formatedName);
